d5ad7aa13b964662c5806c61fa506867aeb0bfe4
Ajay Sahani <sajay3159@gmail.com>
2025-11-14 00:17:53 +0530

    update config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,6 @@
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
 };
 
 export default firebaseConfig;
